a1d5a76bed8871c32f3c6bfdc8ecb6169870df9f
Govind-Jangra <govind70821@gmail.com>
2024-09-13 11:53:26 +0530

    feat: change config file for nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Enabling static export
+    output: 'export', // Tells Next.js to generate static files
+    
+    // Optional: Add this if your deployment platform needs trailing slashes in URLs
+    trailingSlash: true,
+  
+    // Optional: If deploying to a subpath, e.g., GitHub Pages
+    assetPrefix: './',
+  };
+  
+  export default nextConfig;
+  
