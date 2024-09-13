1bcf3aa6e7e032742f7fc3f40301419c9edccb40
Govind-Jangra <govind70821@gmail.com>
2024-09-13 11:59:17 +0530

    refactor config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Enabling static export
-    output: 'export', // Tells Next.js to generate static files
-    
-    // Optional: Add this if your deployment platform needs trailing slashes in URLs
+    assetPrefix: '/',
     trailingSlash: true,
-  
-    // Optional: If deploying to a subpath, e.g., GitHub Pages
-    assetPrefix: './',
-  };
-  
-  export default nextConfig;
-  
+    output: 'export',
+};
+
+export default nextConfig;
